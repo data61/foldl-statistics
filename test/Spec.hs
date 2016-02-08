6bfde7f3c4b7a1b6bdf5cc9566c1edf79f1a53ee
Alex Mason <Alex.Mason@nicta.com.au>
2016-02-08 15:12:56 +1100

    Make tests a little more readable

--- test/Spec.hs
+++ test/Spec.hs
@@ -17,78 +17,85 @@ import Statistics.Function (within)
 toV :: [Double] -> U.Vector Double
 toV = U.fromList
 
+
+onVec :: String -> (U.Vector Double -> QC.Property) -> TestTree
+onVec str f = QC.testProperty str (f . toV)
+
+onVec2 :: String -> (U.Vector (Double,Double) -> QC.Property) -> TestTree
+onVec2 str f = QC.testProperty str (f . U.fromList)
+
 main :: IO ()
 main = defaultMain $
     testGroup "Results match Statistics.Sample"
         [ testGroup "Without pre-computed mean"
             [ testGroup "Statistics of location"
-                [ QC.testProperty "mean" $ \lst -> not (Prelude.null lst) ==>
-                    F.fold mean lst == S.mean (toV lst)
-                , QC.testProperty "meanWeighted" $ \lst -> not (Prelude.null lst) ==>
-                    F.fold meanWeighted lst == S.meanWeighted (U.fromList lst)
-                , QC.testProperty "welfordMean" $ \lst -> not (Prelude.null lst) ==>
-                    F.fold welfordMean lst == S.welfordMean (toV lst)
-                , QC.testProperty "harmonicMean" $ \lst -> not (Prelude.null lst) ==>
-                    F.fold harmonicMean lst == S.harmonicMean (toV lst)
-                , QC.testProperty "geometricMean" $ \lst -> not (Prelude.null lst) ==>
-                    let lst' = map abs lst
-                    in F.fold geometricMean lst' == S.geometricMean (toV lst')
+                [ onVec "mean" $ \vec ->
+                     not (U.null vec) ==> F.fold mean (U.toList vec) == S.mean vec
+                , onVec2 "meanWeighted" $ \vec ->
+                     not (U.null vec) ==> F.fold meanWeighted (U.toList vec) == S.meanWeighted vec
+                , onVec "welfordMean" $ \vec ->
+                     not (U.null vec) ==> F.fold welfordMean (U.toList vec) == S.welfordMean vec
+                , onVec "harmonicMean" $ \vec ->
+                     not (U.null vec) ==> F.fold harmonicMean (U.toList vec) == S.harmonicMean vec
+                , onVec "geometricMean" $ \vec ->
+                     not (U.null vec) ==> let vec' = U.map abs vec
+                        in F.fold geometricMean (U.toList vec') == S.geometricMean vec'
                 ]
 
             , testGroup "Single-pass functions"
-                [ QC.testProperty "fastVariance" $ \lst -> not (Prelude.null lst) ==>
-                    F.fold fastVariance lst == S.fastVariance (toV lst)
-                , QC.testProperty "fastVarianceUnbiased" $ \lst -> not (Prelude.null lst) ==>
-                    F.fold fastVarianceUnbiased lst == S.fastVarianceUnbiased (toV lst)
-                , QC.testProperty "fastStdDev" $ \lst -> not (Prelude.null lst) ==>
-                    F.fold fastStdDev lst == S.fastStdDev (toV lst)
+                [ onVec "fastVariance" $ \vec ->
+                    not (U.null vec) ==> F.fold fastVariance (U.toList vec) == S.fastVariance vec
+                , onVec "fastVarianceUnbiased" $ \vec ->
+                    not (U.null vec) ==> F.fold fastVarianceUnbiased (U.toList vec) == S.fastVarianceUnbiased vec
+                , onVec "fastStdDev" $ \vec ->
+                    not (U.null vec) ==> F.fold fastStdDev (U.toList vec) == S.fastStdDev vec
                 ]
             ]
 
         , testGroup "With pre-computed mean"
             [ testGroup "Functions requiring the mean to be known"
-                [ QC.testProperty "variance" $ \lst -> not (Prelude.null lst) ==>
-                    let m = F.fold mean lst
-                    in F.fold (variance m) lst == S.variance (toV lst)
-                , QC.testProperty "varianceUnbiased" $ \lst -> not (Prelude.null lst) ==>
-                    let m = F.fold mean lst
-                    in F.fold (varianceUnbiased m) lst == S.varianceUnbiased (toV lst)
-                , QC.testProperty "stdDev" $ \lst -> not (Prelude.null lst) ==>
-                    let m = F.fold mean lst
-                    in F.fold (stdDev m) lst == S.stdDev (toV lst)
-                , QC.testProperty "varianceWeighted" $ \lst -> not (Prelude.null lst) ==>
-                    let m = F.fold meanWeighted lst
-                    in F.fold (varianceWeighted m) lst == S.varianceWeighted (U.fromList lst)
+                [ onVec "variance" $ \vec ->
+                    not (U.null vec) ==> let m = F.fold mean (U.toList vec)
+                    in F.fold (variance m) (U.toList vec) == S.variance vec
+                , onVec "varianceUnbiased" $ \vec ->
+                    not (U.null vec) ==> let m = F.fold mean (U.toList vec)
+                    in F.fold (varianceUnbiased m) (U.toList vec) == S.varianceUnbiased vec
+                , onVec "stdDev" $ \vec ->
+                    not (U.null vec) ==> let m = F.fold mean (U.toList vec)
+                    in F.fold (stdDev m) (U.toList vec) == S.stdDev vec
+                , onVec2 "varianceWeighted" $ \vec ->
+                    not (U.null vec) ==> let m = F.fold meanWeighted (U.toList vec)
+                    in F.fold (varianceWeighted m) (U.toList vec) == S.varianceWeighted vec
                 ]
 
             , testGroup "Functions over central moments"
-                [ QC.testProperty "skewness" $ \lst -> length lst > 3 ==>
-                    let m = F.fold mean lst
-                    in F.fold (skewness m) lst == S.skewness (toV lst)
-                , QC.testProperty "kurtosis" $ \lst -> length lst > 4 ==>
-                    let m = F.fold mean lst
-                    in F.fold (kurtosis m) lst == S.kurtosis (toV lst)
-                , QC.testProperty "centralMoment 2" $ \lst ->  length lst > 2 ==>
-                    let m = F.fold mean lst
-                    in F.fold (centralMoment 2 m) lst == S.centralMoment 2 (toV lst)
-                , QC.testProperty "centralMoment 3" $ \lst -> length lst > 3 ==>
-                    let m = F.fold mean lst
-                    in F.fold (centralMoment 3 m) lst == S.centralMoment 3 (toV lst)
-                , QC.testProperty "centralMoment 4" $ \lst -> length lst > 4 ==>
-                    let m = F.fold mean lst
-                    in F.fold (centralMoment 4 m) lst == S.centralMoment 4 (toV lst)
-                , QC.testProperty "centralMoment 7" $ \lst -> length lst > 7 ==>
-                    let m = F.fold mean lst
-                    in F.fold (centralMoment 7 m) lst == S.centralMoment 7 (toV lst)
-                , QC.testProperty "centralMoments 4 9" $ \lst -> length lst > 7 ==>
-                    let m = F.fold mean lst
-                    in F.fold (centralMoments 4 9 m) lst == S.centralMoments 4 9 (toV lst)
+                [ onVec "skewness" $ \vec ->
+                    U.length vec > 3 ==> let m = F.fold mean (U.toList vec)
+                    in F.fold (skewness m) (U.toList vec) == S.skewness vec
+                , onVec "kurtosis" $ \vec ->
+                    U.length vec > 4 ==> let m = F.fold mean (U.toList vec)
+                    in F.fold (kurtosis m) (U.toList vec) == S.kurtosis vec
+                , onVec "centralMoment 2" $ \vec ->
+                     U.length vec > 2 ==> let m = F.fold mean (U.toList vec)
+                    in F.fold (centralMoment 2 m) (U.toList vec) == S.centralMoment 2 vec
+                , onVec "centralMoment 3" $ \vec ->
+                    U.length vec > 3 ==> let m = F.fold mean (U.toList vec)
+                    in F.fold (centralMoment 3 m) (U.toList vec) == S.centralMoment 3 vec
+                , onVec "centralMoment 4" $ \vec ->
+                    U.length vec > 4 ==> let m = F.fold mean (U.toList vec)
+                    in F.fold (centralMoment 4 m) (U.toList vec) == S.centralMoment 4 vec
+                , onVec "centralMoment 7" $ \vec ->
+                    U.length vec > 7 ==> let m = F.fold mean (U.toList vec)
+                    in F.fold (centralMoment 7 m) (U.toList vec) == S.centralMoment 7 vec
+                , onVec "centralMoments 4 9" $ \vec ->
+                    U.length vec > 7 ==> let m = F.fold mean (U.toList vec)
+                    in F.fold (centralMoments 4 9 m) (U.toList vec) == S.centralMoments 4 9 vec
                 -- Cannot test this because we do not have an equivalent implementation
                 -- from the statistics package.
-                -- , QC.testProperty "centralMoments' 4 9" $ \lst -> length lst > 7 ==>
+                -- , onVec "centralMoments' 4 9" $ \vec -> length lst > 7 ==>
                 --     let m = F.fold mean lst
                 --         (f1,f2) = (F.fold (centralMoments' 4 9 m) lst)
-                --         (s1,s2) = (S.centralMoments 4 9 (toV lst))
+                --         (s1,s2) = (S.centralMoments 4 9 vec)
                 --     in within 3 f1 s1 && within 3 f2 s2
                 ]
             ]
